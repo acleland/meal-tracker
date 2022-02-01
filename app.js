78466960d1c9bcd6ead61fa89434796cc5c98147
Andrea Cleland <andreaclelan@gmail.com>
2022-01-31 17:08:54 -0800

    implemented submit ingredient event listener

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 // import functions and grab DOM elements
+const ingredientForm = document.getElementById('ingredientForm');
+const ingredientSubmit = document.getElementById('ingredientSubmit');
 
 // let state
+let ingredients = [];
+let meals = [];
+
+// Display list function
+function displayIngredients() {
+    
+}
 
 // set event listeners 
+ingredientSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const ingredientData = new FormData(ingredientForm);
+    const ingredient = {
+        name: ingredientData.get('ingredient'),
+        quantity: ingredientData.get('quantity'),
+        units: ingredientData.get('units'),
+    };
+    ingredients.push(ingredient);
+    displayIngredients();
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
